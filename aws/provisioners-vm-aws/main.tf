58882f64119c68b1f8805e7bb4d0549949f6ff7d
Douglas Meneses Barbosa <douglasmeneses2015@gmail.com>
2022-07-27 15:37:38 -0300

    Criação provisioners AWS

---
+++ aws/provisioners-vm-aws/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+  backend "s3" {
+    bucket = "douglas-remote-state"
+    key    = "aws-vm-provisioners/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+
+  default_tags {
+    tags = {
+      owner      = "douglasbarbosa"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "douglas-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
